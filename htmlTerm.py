aff3d48ac2bc3c4f0973ca18b47bd8b1a9cd89ab
danielle-galvao <dgalvao@nd.edu>
2020-02-09 16:36:02 -0500

    add function to create pop up window

--- htmlTerm.py
+++ htmlTerm.py
@@ -4,7 +4,11 @@ class Api():
     def commandEntered(self, item):
         print(f'ITEM: {item}')
 
+    def popUp(self, url):
+        wv.create_window('manPage', url, width=400, height=300, resizable=True)
+        wv.start()
+
 if __name__ == '__main__':
     api = Api()
-    wv.create_window('htmlTerm', './index.html', js_api=api, frameless=True)
+    wv.create_window('htmlTerm', './index.html', js_api=api, frameless=False, text_select=True)
     wv.start()

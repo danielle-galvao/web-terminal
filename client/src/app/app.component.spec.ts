696165f7736af50269da1aa96e65cb8f5f5e7dbc
Zoe Surma <zsurma@nd.edu>
2020-04-04 15:07:17 -0400

    Starting out tests

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -19,4 +19,10 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  it(`should have title 'web-terminal'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('web-terminal');
+  });
 });

56dd1a261201b5ff67795e2d102c294179e6b876
John Meyer <jack10042@gmail.com>
2020-04-07 17:55:14 -0400

    0.2.0 (#11)
    * Restructured project and added release actions
    
    * Fix invalid workflow files

---
+++ server/web_terminal/__main__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from .web_terminal import start
+
+if __name__ == '__main__':
+    start()
--- server/web_terminal/server.py
+++ server/web_terminal/server.py
@@ -3,22 +3,32 @@ from random import choice
 from string import ascii_uppercase
 
 app = Flask(__name__)
-token = ''
+
+token_val = ''
+
+def token():
+    global token_val
+
+    if not token_val:
+        token_val = ''.join(choice(ascii_uppercase) for i in range(5))
+
+    return token_val
 
 @app.route('/file/<path:path>', methods=['GET'])
 def local_files(path):
+    #TODO
     return send_from_directory('./', path)
 
 @app.route('/terminal/<path:path>', methods=['GET'])
 def angular_folders(path):
     try:
-        return send_from_directory('./client/dist/web-terminal/', path)
+        return send_from_directory('./client/', path)
     except:
         return redirect('/', code=301)
 
 @app.route('/terminal/')
 def terminal_root():
-        return send_from_directory('./client/dist/web-terminal/', 'index.html')
+        return send_from_directory('./client/', 'index.html')
 
 @app.route('/<path:path>')
 def catch_rest(path):
@@ -29,9 +39,6 @@ def root():
     return redirect('/terminal/', code=301)
 
 def run_flask():
-    global token
-
-    token = ''.join(choice(ascii_uppercase) for i in range(5))
-    print(f'Starting web server, your token is: {token}')
+    print(f'Starting web server, your token is: {token()}')
 
     app.run()

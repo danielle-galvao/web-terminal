359d07faa5bf128db628b5b3b27cf7458d4bbd81
Zoe Surma <zsurma@nd.edu>
2020-04-07 21:27:47 -0400

    Starting backend and history tests

--- client/src/app/backend.service.spec.ts
+++ client/src/app/backend.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BackendService } from './backend.service';
+import { of } from 'rxjs';
 
 describe('BackendService', () => {
   let service: BackendService;
@@ -12,5 +13,23 @@ describe('BackendService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should access with isAuthenticated', () => {
+    expect(service.isAuthenticated()).toEqual(service.authenticated$);
+  });
+
+  it('should try to authenticate', () => {
+    spyOn(console, 'log');
+    service.authenticate('test');
+    expect(console.log).toHaveBeenCalledWith('Submitting token test to backend');
+    // TODO: try to test the pipe result?... maybe export ofType from backend.service
+  });
+
+  it('should try to send to backend', () => {
+    spyOn(console, 'log');
+    service.sendCommand('test');
+    expect(console.log).toHaveBeenCalledWith('Submitting command test to backend');
+    // TODO: try to test the return?
   });
 });
--- client/src/app/terminal/history/history.component.spec.ts
+++ client/src/app/terminal/history/history.component.spec.ts
@@ -1,10 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { Observable, of } from 'rxjs';
 import { HistoryComponent } from './history.component';
+import { BackendService } from 'src/app/backend.service';
 
 describe('HistoryComponent', () => {
   let component: HistoryComponent;
   let fixture: ComponentFixture<HistoryComponent>;
+  let spy;
+  let backendService: BackendService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,9 +22,19 @@ describe('HistoryComponent', () => {
     component = fixture.componentInstance;
     component.commandId = {};
     fixture.detectChanges();
+    backendService = TestBed.inject(BackendService);
+
+    spy = spyOn(backendService, 'getHistoryFor').withArgs(123).and.returnValue('ls');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should toggle output', () => {
+    expect(component.show).toBeFalsy();
+    component.toggleOutput();
+    expect(component.show).toBeTruthy();
+  });
+
 });
